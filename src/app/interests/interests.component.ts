fbaea2e6e257fe194bc7f2975a6c6a432ae25641
Samantha-Varguez <varaguezsamy@gmail.com>
2024-09-23 23:29:14 -0600

    modify interests #26

--- src/app/interests/interests.component.ts
+++ src/app/interests/interests.component.ts
@@ -24,15 +24,11 @@ export class InterestsComponent {
   let ach4 = {
     nombre: "Arte",
   };
-  let ach5 = {
-    nombre: "Idiomas",
-  };
  
    this.interestsExperience.push(ach1);
    this.interestsExperience.push(ach2);
    this.interestsExperience.push(ach3);
    this.interestsExperience.push(ach4);
-   this.interestsExperience.push(ach5);
    console.log(this.interestsExperience);
   }
 }
